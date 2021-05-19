62335907a5c6fc7546ecfb97289f338844d28e01
WALMART4139 <WALMART4139@LAPTOP-IHJQ63LQ>
2021-05-18 20:05:51 -0500

    practica

---
+++ src/videocomunicacion/videoComunicacion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videocomunicacion;
+
+/**
+ *
+ * @author Katia Calan Chi
+ */
+public class videoComunicacion {
+
+    private static String nombre;
+    private static int CantidadUsuarios;
+    private static Integer tiempo;
+    private static String nivel;
+    private static int estrellas;
+    
+    public videoComunicacion(String nombre, int CantidadUsuarios, int tiempo, String nivel, int estrellas) {
+        videoComunicacion.nombre = nombre;
+        videoComunicacion.CantidadUsuarios = CantidadUsuarios;
+        videoComunicacion.estrellas = estrellas;
+        videoComunicacion.tiempo = tiempo;
+        videoComunicacion.nivel = nivel;
+           
+        
+    
+        
+    }
+    
+}
